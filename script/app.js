c725de128fa34ee2c451562a9f27b0ef06a07ec3
john.santiago <john.santiago@urios.edu.ph>
2025-03-27 07:21:41 +0000

    first commit

---
+++ script/app.js
@@ -0,0 +1,9 @@
+Alpine.store("settings", {
+    apiBaseUrl: "http://172.17.100.14:3326/santiago1/api",  
+    appName: "Contact Application",  
+});
+
+Alpine.store("settings", {
+    apiBaseUrl: "http://172.17.100.14:3326/santiago1/api/exam",  
+    appName: "chat Application",  
+});
